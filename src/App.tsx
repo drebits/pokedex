90ca5c723b5d23e669c114cd200ae711fad0b50b
Andres Rosero <andres@andres.sh>
2019-10-26 21:50:36 -0500

    sass colors

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,13 @@ import Grid from '@material-ui/core/Grid'
 import './app.scss'
 import PokemonsList from './components/PokemonsList'
 import Screen from './components/Screen'
+import FakeButtons from './components/FakeButtons'
 
 const App: React.FC = () => (
   <Container maxWidth="md" className="pokedex">
     <Grid container spacing={2}>
       <Grid item xs={12} sm={6}>
-        <div className="buttons-container">
-          <span className="button main" />
-          <span className="button red" />
-          <span className="button yellow" />
-          <span className="button green" />
-        </div>
+        <FakeButtons />
         <Screen />
       </Grid>
       <Grid item xs={12} sm={6}>
---
+++ src/components/FakeButtons/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+import './styles.scss'
+
+const FakeButtons = () => (
+  <div className="fake-buttons">
+    <span className="button main" />
+    <span className="button red" />
+    <span className="button yellow" />
+    <span className="button green" />
+  </div>
+)
+
+export default FakeButtons
