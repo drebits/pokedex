0de3dc9566de04a521a1c4488e0215b30e0590d5
Andres Rosero <andres@andres.sh>
2020-01-24 17:30:51 -0500

    prueba

--- src/App.tsx
+++ src/App.tsx
@@ -15,21 +15,68 @@ interface IHocProps {
   loading: IRootState['loading']
 }
 
-const App: React.FC<IHocProps> = ({ loading }) => (
-  <Container maxWidth="md" className="pokedex">
-    {loading && <Loader />}
-    <Grid container spacing={2}>
-      <Grid item xs={12} sm={6}>
-        <FakeButtons />
-        <Screen />
-        <PokemonData />
-      </Grid>
-      <Grid item xs={12} sm={6}>
-        <PokemonsList />
-      </Grid>
-    </Grid>
-  </Container>
-)
+interface IAppState {
+  connectedToParent: boolean
+}
+
+const CONTAINER_APP_URL = 'http://localhost:3001'
+
+class App extends React.Component<IHocProps, IAppState> {
+  constructor(props: IHocProps) {
+    super(props)
+
+    this.state = {
+      connectedToParent: false
+    }
+  }
+
+  public componentDidMount() {
+    const parentWindow = window.parent
+    window.addEventListener('message', this.receiveMessage)
+
+    parentWindow.postMessage('hello-parent', CONTAINER_APP_URL)
+  }
+
+  public componentWillUnmount() {
+    window.removeEventListener('message', this.receiveMessage)
+  }
+
+  public render() {
+    const { loading } = this.props
+
+    return (
+      <Container maxWidth="md" className="pokedex">
+        {loading && <Loader />}
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
+            <FakeButtons />
+            <Screen />
+            <PokemonData />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <PokemonsList />
+          </Grid>
+        </Grid>
+      </Container>
+    )
+  }
+
+  private receiveMessage = (event: MessageEvent) => {
+    const { connectedToParent } = this.state
+
+    if (
+      !connectedToParent &&
+      event.origin === CONTAINER_APP_URL &&
+      event.data === 'hello-children'
+    ) {
+      if (event.source) {
+        // @ts-ignore
+        event.source.postMessage('hello-parent', event.origin)
+      }
+      this.setState({ connectedToParent: true })
+    }
+  }
+}
 
 const mapStateToProps = ({ loading }: IRootState) => ({
   loading
